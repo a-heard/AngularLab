1a4f18020e647d7a5cac5a5ebe76bb82beaf3cf7
a-heard <heardaisha@gmail.com>
2020-05-27 23:08:45 -0400

    second day lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompOneComponent } from './comp-one/comp-one.component';
+import { CompTwoComponent } from './comp-two/comp-two.component';
+import { FilterComponent } from './filter/filter.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompOneComponent,
+    CompTwoComponent,
+    FilterComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
